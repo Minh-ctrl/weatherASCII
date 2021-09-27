72ee482fbd4408a179e3a957bcebb9492a8f09b2
Minh-ctrl <binhminh140899@gmail.com>
2021-09-28 04:54:12 +0900

    working on text input now

--- src/lib/weatherASCII.svelte
+++ src/lib/weatherASCII.svelte
@@ -1,26 +1,43 @@
 <script lang="ts">
     import objASCII from "$lib/weatherConstant";
     import type {WeatherData} from "src/global";
-    let location="india";
+    import { createEventDispatcher } from 'svelte';
+    const dispatch= createEventDispatcher();
+    let location;
     let ASCIIicon;
+    let fetched;
     async function fetchWeatherData(){
         const req= await fetch (`http://wttr.in/${location}?format=j1`);
         const res= (await req.json()) as WeatherData;
         let weathercode= res.current_condition[0].weatherCode;
         ASCIIicon=objASCII[weathercode]
+        fetched= res;
+        console.log(fetched);
         return res;
     }
     let promise= fetchWeatherData();
+    function dispatcher(){
+        dispatch('')
+    }
 </script>
 
-<pre class="text-gray-200 flex flex-col leading-tight">
- Current Weather:
+<pre class="text-gray-200 flex flex-row leading-tight">
 {#await promise}
-    <p> fetching data</p>
+    <input type="text" bind:value={location}>
     {:then weatherData}
+    <div class="container flex flex-col w-1/12">
     {#each ASCIIicon.weatherSymbol as weatherSymbol}
         <span class="symbol">{weatherSymbol}</span>
     {/each} 
+    </div>
+    <div class="container flex flex-col">
+    <p>Weather report: {location.toLocaleUpperCase()}</p>
+    <p>{fetched.current_condition[0].weatherDesc[0].value}</p>
+    <pre class="container flex flex-row">
+        <p>{fetched.current_condition[0].FeelsLikeC}</p>
+        <p>°C</p>
+    </pre>
+</div>
 {/await}
 </pre>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
     import WeatherAscii from "$lib/weatherASCII.svelte";
 
 </script>
-<main class="w-full h-screen bg-gray-900">
+<main class="w-full h-screen bg-black">
 <WeatherAscii></WeatherAscii>
 
 
