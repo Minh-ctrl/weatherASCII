87b7e59d79afdc01146207f363f76da944b4cbfa
minh-ctrl <binhminh140899@gmail.com>
2021-12-06 12:00:19 +0900

    refactor: 💡 refactored unecessary API handler

--- src/lib/APIHandler.ts
+++
@@ -1,9 +0,0 @@
-import type { WeatherData } from "src/global";
-
-export let location="japan";
-async function fetchWeatherData(){
-    const req= await fetch (`http://wttr.in/${location}?format=j1`);
-    const res= (await req.json()) as WeatherData;
-    return res;
-}
-console.log(fetchWeatherData())
