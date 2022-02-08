0096732f3143bb8c77666c2aa4499eeaceaec48c
minh-ctrl <binhminh140899@gmail.com>
2022-02-09 00:29:49 +0900

    formatting

--- src/lib/weatherASCII.svelte
+++ src/lib/weatherASCII.svelte
@@ -1,110 +1,137 @@
 <script lang="ts">
     import objASCII from "$lib/weatherConstant";
-    import type {WeatherData} from "src/global";
-    let weatherCode, weatherCode1 ,weatherCode2;
+    import type { WeatherData } from "src/global";
+    let weatherCode, weatherCode1, weatherCode2;
     let fetched;
-    let arr=[];
-    let newline='>'; 
-    async function fetchWeatherData(){
-        const req = await fetch (`https://wttr.in/${newline}?format=j1`);
+    let arr = [];
+    let newline = ">";
+    async function fetchWeatherData() {
+        const req = await fetch(`https://wttr.in/${newline}?format=j1`);
         const res = (await req.json()) as WeatherData;
         weatherCode = res.current_condition[0].weatherCode;
         weatherCode1 = res.weather[1].hourly[5].weatherCode;
         weatherCode2 = res.weather[2].hourly[5].weatherCode;
-        fetched= res;
+        fetched = res;
         return res;
     }
-    async function handleKeydown(event){  
-        if (event.code == 'Enter'){
-            event.preventDefault()
+    async function handleKeydown(event) {
+        if (event.code == "Enter") {
+            event.preventDefault();
             await fetchWeatherData();
-            arr= [...arr, {
-                text: newline, 
-                fetched,  
-                ASCIIicon: objASCII[weatherCode],
-                ASCIIicon1: objASCII[weatherCode1],
-                ASCIIicon2: objASCII[weatherCode2]
-            }];
-            newline='>';
+            arr = [
+                ...arr,
+                {
+                    text: newline,
+                    fetched,
+                    ASCIIicon: objASCII[weatherCode],
+                    ASCIIicon1: objASCII[weatherCode1],
+                    ASCIIicon2: objASCII[weatherCode2],
+                },
+            ];
+            newline = ">";
         }
     }
 </script>
+
 <main class="w-full flex flex-col justify-between">
     <div class="container flex flex-col flex-nowrap justify-center w-full">
         {#each arr as query}
-        <div class="history text-white bg-black text-4xl w-full">{query.text}</div>
-                <pre class="w-full flex flex-row leading-tight justify-around">
+            <div class="history text-white bg-black text-4xl w-full">
+                {query.text}
+            </div>
+            <pre class="w-full flex flex-row leading-tight justify-around">
                     <div class="container flex flex-row w-full justify-around">
                         <div class="grid">
                             <div class="container flex flex-col w-full">
                                 {#each query.ASCIIicon.weatherSymbol as weatherSymbol}
-                                    <span class="symbol text-xl w-full">{weatherSymbol}</span>
-                                {/each} 
+                                <span class="symbol text-xl w-full"
+                                    >{weatherSymbol}</span
+                                >
+                            {/each} 
                             </div>
                             <div class="container text-xl flex flex-col">
-                                <p>Country: {query.fetched.nearest_area[0].country[0].value}</p>
-                                <p>{query.fetched.current_condition[0].localObsDateTime}</p>
-                                <p>{query.fetched.current_condition[0].weatherDesc[0].value}</p>
-                                <p>{query.fetched.current_condition[0].FeelsLikeC}°C</p>
+                                <p>Country: {query.fetched.nearest_area[0]
+                                    .country[0].value}</p>
+                                <p>{query.fetched.current_condition[0]
+                                    .localObsDateTime}</p>
+                                <p>{query.fetched.current_condition[0]
+                                    .weatherDesc[0].value}</p>
+                                <p>{query.fetched.current_condition[0]
+                                    .FeelsLikeC}°C</p>
                             </div>
                         </div>
                         <div class="grid">
                             <div class="container flex flex-col">
                                 {#each query.ASCIIicon1.weatherSymbol as weatherSymbol}
-                                    <span class="symbol text-xl">{weatherSymbol}</span>
-                                {/each} 
+                                <span class="symbol text-xl"
+                                    >{weatherSymbol}</span
+                                >
+                            {/each} 
                             </div>
                             <div class="container text-xl flex flex-col">
                                 <p>{query.fetched.weather[1].date}</p>
-                                <p>{query.fetched.weather[1].hourly[5].weatherDesc[0].value}</p>
-                                <p>{query.fetched.weather[1].hourly[5].FeelsLikeC}°C</p>
+                                <p>{query.fetched.weather[1].hourly[5]
+                                    .weatherDesc[0].value}</p>
+                                <p>{query.fetched.weather[1].hourly[5]
+                                    .FeelsLikeC}°C</p>
                             </div>
                         </div>
                         <div class="grid">
                             <div class="container flex flex-col w-full">
                                 {#each query.ASCIIicon2.weatherSymbol as weatherSymbol}
-                                    <span class="symbol text-xl">{weatherSymbol}</span>
-                                {/each} 
+                                <span class="symbol text-xl"
+                                    >{weatherSymbol}</span
+                                >
+                            {/each} 
                             </div>
                             <div class="container text-xl flex flex-col">
                                 <p>{query.fetched.weather[2].date}</p>
-                                <p>{query.fetched.weather[2].hourly[5].weatherDesc[0].value}</p>
-                                <p>{query.fetched.weather[2].hourly[5].FeelsLikeC}°C</p>
+                                <p>{query.fetched.weather[2].hourly[5]
+                                    .weatherDesc[0].value}</p>
+                                <p>{query.fetched.weather[2].hourly[5]
+                                    .FeelsLikeC}°C</p>
                             </div>
                         </div>
                     </div>
                 </pre>
-            {/each}
+        {/each}
     </div>
     <div class="input__field flex flex-row justify-center w-full  text-3xl">
-        <input bind:value={newline}  contenteditable="true" class="bg-black w-full inline outline-none visible" type="text" on:keyup|preventDefault={handleKeydown} >
+        <input
+            bind:value={newline}
+            contenteditable="true"
+            class="bg-black w-full inline outline-none visible"
+            type="text"
+            on:keyup|preventDefault={handleKeydown}
+        />
     </div>
 
-<div class="w-full flex flex-row justify-center h-full">
-    <pre class="w-full flex flex-row leading-tight justify-around">
+    <div class="w-full flex flex-row justify-center h-full">
+        <pre class="w-full flex flex-row leading-tight justify-around">
     {#await handleKeydown}
-    <div class="history text-white  text-5xl w-full "> FETCHING DATA</div>
-        {:then weatherData}
-        <!-- this section is left empty because it is redundant as the data is displayed on the pre tag -->
-    {/await}
+                <div
+                    class="history text-white  text-5xl w-full "> FETCHING DATA</div>
+            {:then weatherData}
+                <!-- this section is left empty because it is redundant as the data is displayed on the pre tag -->
+            {/await}
     </pre>
-</div>
+    </div>
 </main>
+
 <style>
-    .symbol{
-    letter-spacing: -0.05em;
+    .symbol {
+        letter-spacing: -0.05em;
     }
-    :root{
+    :root {
         --text-color: #00aa00;
     }
-    pre{
+    pre {
         color: var(--text-color);
     }
-    input{
+    input {
         color: var(--text-color);
     }
-    .history{
+    .history {
         color: var(--text-color);
     }
-
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,131 +1,199 @@
-
 <script>
     import WeatherAscii from "$lib/weatherASCII.svelte";
 </script>
+
 <main class="crt w-full h-full">
-<WeatherAscii/>
+    <WeatherAscii />
 </main>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
-    main{
+    @import url("https://fonts.googleapis.com/css2?family=VT323&display=swap");
+    main {
         font-family: VT323;
         text-transform: uppercase;
     }
     .crt::before {
-    background: linear-gradient(#101010 50%, rgba(16, 16, 16, 0.2) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.03), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.03));
-	background-size: 100% 3px, 6px 100%;
-	content: "";
-	pointer-events: none;
-	position: fixed;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-	z-index: 2;
+        background: linear-gradient(#101010 50%, rgba(16, 16, 16, 0.2) 50%),
+            linear-gradient(
+                90deg,
+                rgba(255, 0, 0, 0.03),
+                rgba(0, 255, 0, 0.02),
+                rgba(0, 0, 255, 0.03)
+            );
+        background-size: 100% 3px, 6px 100%;
+        content: "";
+        pointer-events: none;
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 2;
     }
     .crt::after {
         animation: flicker 0 infinite;
-	    background: rgba(16, 16, 16, 0.2);
-	    content: "";
-	    pointer-events: none;
-	    position: fixed;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-	    z-index: 2;
+        background: rgba(16, 16, 16, 0.2);
+        content: "";
+        pointer-events: none;
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 2;
     }
-    .crt{
+    .crt {
         animation: textShadow 1s infinite;
     }
     @keyframes textShadow {
-    0% {
-        text-shadow: 0.4389924193300864px 0 1px rgba(0,30,255,0.5), -0.4389924193300864px 0 1px rgba(255,0,80,0.3), 0 0 3px;
+        0% {
+            text-shadow: 0.4389924193300864px 0 1px rgba(0, 30, 255, 0.5),
+                -0.4389924193300864px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        5% {
+            text-shadow: 2.7928974010788217px 0 1px rgba(0, 30, 255, 0.5),
+                -2.7928974010788217px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        10% {
+            text-shadow: 0.02956275843481219px 0 1px rgba(0, 30, 255, 0.5),
+                -0.02956275843481219px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        15% {
+            text-shadow: 0.40218538552878136px 0 1px rgba(0, 30, 255, 0.5),
+                -0.40218538552878136px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        20% {
+            text-shadow: 3.4794037899852017px 0 1px rgba(0, 30, 255, 0.5),
+                -3.4794037899852017px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        25% {
+            text-shadow: 1.6125630401149584px 0 1px rgba(0, 30, 255, 0.5),
+                -1.6125630401149584px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        30% {
+            text-shadow: 0.7015590085143956px 0 1px rgba(0, 30, 255, 0.5),
+                -0.7015590085143956px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        35% {
+            text-shadow: 3.896914047650351px 0 1px rgba(0, 30, 255, 0.5),
+                -3.896914047650351px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        40% {
+            text-shadow: 3.870905614848819px 0 1px rgba(0, 30, 255, 0.5),
+                -3.870905614848819px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        45% {
+            text-shadow: 2.231056963361899px 0 1px rgba(0, 30, 255, 0.5),
+                -2.231056963361899px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        50% {
+            text-shadow: 0.08084290417898504px 0 1px rgba(0, 30, 255, 0.5),
+                -0.08084290417898504px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        55% {
+            text-shadow: 2.3758461067427543px 0 1px rgba(0, 30, 255, 0.5),
+                -2.3758461067427543px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        60% {
+            text-shadow: 2.202193051050636px 0 1px rgba(0, 30, 255, 0.5),
+                -2.202193051050636px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        65% {
+            text-shadow: 2.8638780614874975px 0 1px rgba(0, 30, 255, 0.5),
+                -2.8638780614874975px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        70% {
+            text-shadow: 0.48874025155497314px 0 1px rgba(0, 30, 255, 0.5),
+                -0.48874025155497314px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        75% {
+            text-shadow: 1.8948491305757957px 0 1px rgba(0, 30, 255, 0.5),
+                -1.8948491305757957px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        80% {
+            text-shadow: 0.0833037308038857px 0 1px rgba(0, 30, 255, 0.5),
+                -0.0833037308038857px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        85% {
+            text-shadow: 0.09769827255241735px 0 1px rgba(0, 30, 255, 0.5),
+                -0.09769827255241735px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        90% {
+            text-shadow: 3.443339761481782px 0 1px rgba(0, 30, 255, 0.5),
+                -3.443339761481782px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        95% {
+            text-shadow: 2.1841838852799786px 0 1px rgba(0, 30, 255, 0.5),
+                -2.1841838852799786px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
+        100% {
+            text-shadow: 2.6208764473832513px 0 1px rgba(0, 30, 255, 0.5),
+                -2.6208764473832513px 0 1px rgba(255, 0, 80, 0.3), 0 0 3px;
+        }
     }
-    5% {
-        text-shadow: 2.7928974010788217px 0 1px rgba(0,30,255,0.5), -2.7928974010788217px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    10% {
-        text-shadow: 0.02956275843481219px 0 1px rgba(0,30,255,0.5), -0.02956275843481219px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    15% {
-        text-shadow: 0.40218538552878136px 0 1px rgba(0,30,255,0.5), -0.40218538552878136px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    20% {
-        text-shadow: 3.4794037899852017px 0 1px rgba(0,30,255,0.5), -3.4794037899852017px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    25% {
-        text-shadow: 1.6125630401149584px 0 1px rgba(0,30,255,0.5), -1.6125630401149584px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    30% {
-        text-shadow: 0.7015590085143956px 0 1px rgba(0,30,255,0.5), -0.7015590085143956px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    35% {
-        text-shadow: 3.896914047650351px 0 1px rgba(0,30,255,0.5), -3.896914047650351px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    40% {
-        text-shadow: 3.870905614848819px 0 1px rgba(0,30,255,0.5), -3.870905614848819px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    45% {
-        text-shadow: 2.231056963361899px 0 1px rgba(0,30,255,0.5), -2.231056963361899px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    50% {
-        text-shadow: 0.08084290417898504px 0 1px rgba(0,30,255,0.5), -0.08084290417898504px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    55% {
-        text-shadow: 2.3758461067427543px 0 1px rgba(0,30,255,0.5), -2.3758461067427543px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    60% {
-        text-shadow: 2.202193051050636px 0 1px rgba(0,30,255,0.5), -2.202193051050636px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    65% {
-        text-shadow: 2.8638780614874975px 0 1px rgba(0,30,255,0.5), -2.8638780614874975px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    70% {
-        text-shadow: 0.48874025155497314px 0 1px rgba(0,30,255,0.5), -0.48874025155497314px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    75% {
-        text-shadow: 1.8948491305757957px 0 1px rgba(0,30,255,0.5), -1.8948491305757957px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    80% {
-        text-shadow: 0.0833037308038857px 0 1px rgba(0,30,255,0.5), -0.0833037308038857px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    85% {
-        text-shadow: 0.09769827255241735px 0 1px rgba(0,30,255,0.5), -0.09769827255241735px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    90% {
-        text-shadow: 3.443339761481782px 0 1px rgba(0,30,255,0.5), -3.443339761481782px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    95% {
-        text-shadow: 2.1841838852799786px 0 1px rgba(0,30,255,0.5), -2.1841838852799786px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-    100% {
-        text-shadow: 2.6208764473832513px 0 1px rgba(0,30,255,0.5), -2.6208764473832513px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-    }
-}
     @keyframes flicker {
-        0% { opacity: 0.15795 }
-		5% { opacity: 0.31511 }
-		10% { opacity: 0.94554 }
-		15% { opacity: 0.2469 }
-		20% { opacity: 0.62031 }
-		25% { opacity: 0.0293 }
-		30% { opacity: 0.00899 }
-		35% { opacity: 0.5344 }
-		40% { opacity: 0.12778 }
-		45% { opacity: 0.52042 }
-		50% { opacity: 0.3823 }
-		55% { opacity: 0.2198 }
-		60% { opacity: 0.9383 }
-		65% { opacity: 0.86615 }
-		70% { opacity: 0.68695 }
-		75% { opacity: 0.55749 }
-		80% { opacity: 0.96984 }
-		85% { opacity: 0.0361 }
-		90% { opacity: 0.24467 }
-		95% { opacity: 0.08351 }
-		100% { opacity: 0.54813 }
-}
-
+        0% {
+            opacity: 0.15795;
+        }
+        5% {
+            opacity: 0.31511;
+        }
+        10% {
+            opacity: 0.94554;
+        }
+        15% {
+            opacity: 0.2469;
+        }
+        20% {
+            opacity: 0.62031;
+        }
+        25% {
+            opacity: 0.0293;
+        }
+        30% {
+            opacity: 0.00899;
+        }
+        35% {
+            opacity: 0.5344;
+        }
+        40% {
+            opacity: 0.12778;
+        }
+        45% {
+            opacity: 0.52042;
+        }
+        50% {
+            opacity: 0.3823;
+        }
+        55% {
+            opacity: 0.2198;
+        }
+        60% {
+            opacity: 0.9383;
+        }
+        65% {
+            opacity: 0.86615;
+        }
+        70% {
+            opacity: 0.68695;
+        }
+        75% {
+            opacity: 0.55749;
+        }
+        80% {
+            opacity: 0.96984;
+        }
+        85% {
+            opacity: 0.0361;
+        }
+        90% {
+            opacity: 0.24467;
+        }
+        95% {
+            opacity: 0.08351;
+        }
+        100% {
+            opacity: 0.54813;
+        }
+    }
 </style>
